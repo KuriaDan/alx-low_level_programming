8cb6e725acb950d1c47c6b2f2f47a1ae44ab151a
KuriaDan <thuku.kuria@live.com>
2022-06-30 08:09:38 -0700

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+  * main - print a string using the printf function followed by a new line
+  * Return: 0
+  */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome ia a piece of art, \n");
+	return (0);
+}
