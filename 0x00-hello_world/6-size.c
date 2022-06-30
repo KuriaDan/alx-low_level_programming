dfdf25432d191b537ec689c25d5956487d22c549
KuriaDan <thuku.kuria@live.com>
2022-06-30 08:30:47 -0700

    size of variables

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+  * main - print size of variables
+  * This program prints size of variables in a computer it's run on
+  * Return: 0
+  */
+
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(c));
+	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("Size of long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a float: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
