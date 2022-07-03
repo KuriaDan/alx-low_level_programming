3c226e14fecf9c2bb9d2fa4a869c8515a61444f0
KuriaDan <thuku.kuria@live.com>
2022-07-03 03:37:54 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - print all lowercase and uppercase letters.
+  * Return: 0
+  */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
