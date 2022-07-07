e796402984c88d43426cb0eac900cc71f09cb984
Kuria_Dan <thuku.kuria@live.com>
2022-07-07 06:25:19 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  * _isupper - function to check upper or lower case
+  * @c: int parameter of the function
+  * Return: 0 or 1
+  */
+int _isuper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return(1);
+	}
+	else
+		return (0);
+}
