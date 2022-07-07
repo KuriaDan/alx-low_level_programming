e1c25cd0391c0c2db46a2fe3066cd3944471722b
Kuria_Dan <thuku.kuria@live.com>
2022-07-07 07:21:32 -0700

    putchar numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+  * print_numbers - function to print numbers
+  * Return: 0
+  */
+void print_numbers(void)
+{
+	char c = '0';
+
+	while (c <= '9')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
