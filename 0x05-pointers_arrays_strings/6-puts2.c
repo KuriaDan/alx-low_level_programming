f8307681201154c6dc3a7ece96d30e38eb58ac88
Kuria_Dan <thuku.kuria@live.com>
2022-07-11 10:11:47 -0700

    every other

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+  * puts2 - function to print every other character of a string
+  * @str: string to manipulate
+  * Return: 0
+  */
+void puts2(char *str)
+{
+	int c;
+	char l;
+
+	for (c = 0; str[c] != 0; c++)
+	{
+		if (c % 2 == 0)
+		{
+			l = str[c];
+			_putchar(l)
+		}
+	}
+	_putchar('\n');
+}
