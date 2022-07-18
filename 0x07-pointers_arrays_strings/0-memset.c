8cdefed7eb0f9845edb9d63801cbea0e7d63e824
Kuria_Dan <thuku.kuria@live.com>
2022-07-18 03:07:42 -0700

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+  * _memset - fill the first n bytes of memory pointed by s with b
+  * @s: array
+  * @b: constant byte
+  * @n: number of times to change value in memory
+  * Return: string s
+  */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a = 0;
+
+	while (a < b)
+	{
+		s[a] = b;
+		a++;
+	}
+	return (s);
+}
