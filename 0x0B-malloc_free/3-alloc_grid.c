b221f9a0233e94f4c70c45391a42da7151980e09
Kuria_dan <thuku.kuria@live.com>
2022-07-26 16:04:32 +0300

    2D

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,9 +26,9 @@ int **alloc_grid(int width, int height)
 				free(arr[j]);
 				j--;
 			}
+			free(arr);
+			return (NULL);
 		}
-		free(arr);
-		return (NULL);
 	}
 	for (j = 0; j < height; j++)
 	{
