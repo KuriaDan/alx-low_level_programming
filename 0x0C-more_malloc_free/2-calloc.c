0353e48f5124ca45066a844044102636c7753a82
Kuria_dan <thuku.kuria@live.com>
2022-07-29 16:26:21 +0300

    frustrations

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -8,15 +8,15 @@
   */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *p;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ptr = malloc(nmemb * size);
-	if (ptr == NULL)
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
-		ptr[i] = 0;
-	return (ptr);
+		p[i] = 0;
+	return (p);
 }
